9320dfa9588f4e8dee3397891eaa09ca1384f3e7
xnorlogic <xnorlogic@gmail.com>
2015-10-06 11:39:52 -0500

    Create PIC16F73_4_BIT_CNT_types.h

---
+++ lib/Suppor-Files/PIC16F73_4_BIT_CNT_types.h
@@ -0,0 +1,29 @@
+/*
+ * File: PIC16F73_4_BIT_CNT_types.h
+ *
+ * Code generated for Simulink model 'PIC16F73_4_BIT_CNT'.
+ *
+ * Model version                  : 1.22
+ * Simulink Coder version         : 8.1 (R2011b) 08-Jul-2011
+ * TLC version                    : 8.1 (Jul  9 2011)
+ * C/C++ source code generated on : Wed Jan 28 11:05:22 2015
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Generic->8-bit Embedded Processor
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_PIC16F73_4_BIT_CNT_types_h_
+#define RTW_HEADER_PIC16F73_4_BIT_CNT_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct RT_MODEL_PIC16F73_4_BIT_CNT RT_MODEL_PIC16F73_4_BIT_CNT;
+
+#endif                                 /* RTW_HEADER_PIC16F73_4_BIT_CNT_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
