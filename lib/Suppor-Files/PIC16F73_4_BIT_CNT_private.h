6d9595c1be0bb206b5e5cfaab9b6becbf4ddf98b
xnorlogic <xnorlogic@gmail.com>
2015-10-06 11:39:35 -0500

    Create PIC16F73_4_BIT_CNT_private.h

---
+++ lib/Suppor-Files/PIC16F73_4_BIT_CNT_private.h
@@ -0,0 +1,40 @@
+/*
+ * File: PIC16F73_4_BIT_CNT_private.h
+ *
+ * Code generated for Simulink model 'PIC16F73_4_BIT_CNT'.
+ *
+ * Model version                  : 1.22
+ * Simulink Coder version         : 8.1 (R2011b) 08-Jul-2011
+ * TLC version                    : 8.1 (Jul  9 2011)
+ * C/C++ source code generated on : Wed Jan 28 11:05:22 2015
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Generic->8-bit Embedded Processor
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_PIC16F73_4_BIT_CNT_private_h_
+#define RTW_HEADER_PIC16F73_4_BIT_CNT_private_h_
+#include "rtwtypes.h"
+#define CALL_EVENT                     (-1)
+#ifndef __RTWTYPES_H__
+#error This file requires rtwtypes.h to be included
+#else
+#ifdef TMWTYPES_PREVIOUSLY_INCLUDED
+#error This file requires rtwtypes.h to be included before tmwtypes.h
+#else
+
+/* Check for inclusion of an incorrect version of rtwtypes.h */
+#ifndef RTWTYPES_ID_C08S16I16L32N08F0
+#error This code was generated with a different "rtwtypes.h" than the file included
+#endif                                 /* RTWTYPES_ID_C08S16I16L32N08F0 */
+#endif                                 /* TMWTYPES_PREVIOUSLY_INCLUDED */
+#endif                                 /* __RTWTYPES_H__ */
+#endif                                 /* RTW_HEADER_PIC16F73_4_BIT_CNT_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
